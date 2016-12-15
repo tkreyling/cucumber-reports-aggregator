8b24866319f6a626c3e2f811fdfa8fd31bff6b7e
Kreyling, Thomas (ext) <thomas.kreyling.ex@easycredit.de>
2016-12-15 15:29:00 +0100

    added fetching of multiple reports and extracted JenkinsRequestProcessor
    * added fetching of multiple reports with ParallelBatch
    * extracted request handling in processor class which has context as property so not each method needs to pass this paramter

--- src/main/java/kreyling/cragg/Main.java
+++ src/main/java/kreyling/cragg/Main.java
@@ -1,33 +1,59 @@
 package kreyling.cragg;
 
+import ratpack.exec.Promise;
+import ratpack.exec.util.ParallelBatch;
 import ratpack.handling.Context;
+import ratpack.http.MediaType;
+import ratpack.http.Status;
 import ratpack.http.client.HttpClient;
+import ratpack.http.client.ReceivedResponse;
 import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
 
-    public static final String JENKINS_REPORT = "https://jenkins.easycredit.intern/view/KW-B2B/view/kwb2b/job/kwb2b-tests/1322/cucumber-html-reports/";
+    public static final String JENKINS_JOB = "https://jenkins.easycredit.intern/view/KW-B2B/view/kwb2b/job/kwb2b-tests/";
+    public static final String CUCUMBER_REPORT = "/cucumber-html-reports/feature-overview.html";
 
     public static void main(String... args) throws Exception {
         RatpackServer.start(server -> server
             .serverConfig(c -> c.baseDir(BaseDir.find()).build())
             .handlers(chain -> chain
                     .files(files -> files.dir("static"))
-                    .get(Main::handleRequest)
+                    .get(context -> new JenkinsRequestProcessor(context).process())
             )
         );
     }
 
-    private static void handleRequest(Context ctx) {
-        HttpClient httpClient = ctx.get(HttpClient.class);
-        URI uri = URI.create(JENKINS_REPORT + "feature-overview.html");
+    private static class JenkinsRequestProcessor {
+        private final Context context;
 
-        httpClient.get(uri).then(response -> {
-            ctx.getResponse().status(response.getStatus()).contentType(response.getBody().getContentType().toString());
-            ctx.render(response.getBody().getText());
-        });
+        public JenkinsRequestProcessor(Context context) {
+            this.context = context;
+        }
+
+        public void process() {
+            HttpClient httpClient = context.get(HttpClient.class);
+
+            ParallelBatch.of(Stream.of(1322, 1321, 1319)
+                .map(build -> URI.create(JENKINS_JOB + build + CUCUMBER_REPORT))
+                .map(httpClient::get)
+                .map(promise -> promise.map(receivedResponse ->  receivedResponse.getBody().getText()))
+                .collect(Collectors.toList()))
+                .yield()
+                .then(this::mapJenkinsResponses);
+        }
+
+        private void mapJenkinsResponses(List<? extends String> responses) {
+            String response = responses.get(0);
+            context.getResponse().status(Status.OK).contentType(MediaType.TEXT_HTML);
+            context.render(response);
+        }
+
     }
 }
