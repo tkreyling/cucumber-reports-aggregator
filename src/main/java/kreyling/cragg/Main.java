8a981171ec464a993fef2de7d024717fdeed37dd
Kreyling, Thomas (ext) <thomas.kreyling.ex@easycredit.de>
2016-12-15 13:45:54 +0100

    switched to request param

--- src/main/java/kreyling/cragg/Main.java
+++ src/main/java/kreyling/cragg/Main.java
@@ -3,12 +3,11 @@ package kreyling.cragg;
 import ratpack.server.RatpackServer;
 
 public class Main {
- public static void main(String... args) throws Exception {
-   RatpackServer.start(server -> server 
-     .handlers(chain -> chain
-       .get(ctx -> ctx.render("Hello World!"))
-       .get(":name", ctx -> ctx.render("Hello " + ctx.getPathTokens().get("name") + "!"))
-     )
-   );
- }
+    public static void main(String... args) throws Exception {
+        RatpackServer.start(server -> server
+            .handlers(chain -> chain
+                .get(ctx -> ctx.render("Hello " + ctx.getRequest().getQueryParams().get("name") + "!"))
+            )
+        );
+    }
 }
