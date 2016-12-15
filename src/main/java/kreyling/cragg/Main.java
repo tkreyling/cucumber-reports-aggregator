686ee7fba67165f86d2a24971c79d02323199d08
Kreyling, Thomas (ext) <thomas.kreyling.ex@easycredit.de>
2016-12-15 18:41:41 +0100

    added matching of features and generation of aggregated report
    * added matching of features across different test reports
    * added generation of aggregated report from matched features

--- src/main/java/kreyling/cragg/Main.java
+++ src/main/java/kreyling/cragg/Main.java
@@ -1,9 +1,12 @@
 package kreyling.cragg;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
 import lombok.Value;
+import lombok.experimental.NonFinal;
 import ratpack.exec.util.ParallelBatch;
 import ratpack.handling.Context;
 import ratpack.http.MediaType;
@@ -24,6 +27,7 @@ import org.jdom2.xpath.XPathFactory;
 import java.io.StringReader;
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 public class Main {
@@ -41,14 +45,40 @@ public class Main {
         );
     }
 
-    @Value
+    @Value @NonFinal
     private static class TestReportLine {
         String feature;
         String status;
     }
 
+    private static class NullTestReportLine extends TestReportLine{
+        public NullTestReportLine(String feature) {
+            super(feature, "");
+        }
+    }
+
     @Value
     private static class TestReport {
+        private final Map<String, List<TestReportLine>> testReportLinesByFeature;
+
+        public TestReport(List<TestReportLine> testReportLines) {
+            testReportLinesByFeature = testReportLines.stream().collect(groupingBy(TestReportLine::getFeature));
+        }
+
+        public Stream<String> getAllFeatures() {
+            return testReportLinesByFeature.keySet().stream();
+        }
+
+        public TestReportLine getTestReportLineByFeature(String feature) {
+            if (!testReportLinesByFeature.containsKey(feature)) return new NullTestReportLine(feature);
+
+            return testReportLinesByFeature.get(feature).get(0);
+        }
+    }
+
+    @Value
+    private static class AggregatedTestReportLine {
+        String feature;
         List<TestReportLine> testReportLines;
     }
 
@@ -125,7 +155,51 @@ public class Main {
 
         private void renderTestReports(List<? extends TestReport> testReports) {
             context.getResponse().status(Status.OK).contentType(MediaType.TEXT_HTML);
-            context.render(testReports.get(0).getTestReportLines().get(0).toString());
+
+            List<AggregatedTestReportLine> aggregatedTestReportLines = testReports.stream()
+                .flatMap(TestReport::getAllFeatures)
+                .distinct()
+                .map(feature -> createAggregatedTestReportLine(testReports, feature))
+                .sorted(comparing(AggregatedTestReportLine::getFeature))
+                .collect(toList());
+
+            StringBuilder response = new StringBuilder();
+            response.append("<html>");
+            response.append("<head>");
+            response.append("<link rel=\"stylesheet\" href=\"css/bootstrap.min.css\" type=\"text/css\"/>");
+            response.append("<link rel=\"stylesheet\" href=\"css/reporting.css\" type=\"text/css\"/>");
+            response.append("<link rel=\"stylesheet\" href=\"css/font-awesome.min.css\"/>");
+            response.append("</head>");
+            response.append("<body>");
+            response.append("<table class=\"stats-table table-hover\">");
+            aggregatedTestReportLines.forEach(aggregatedTestReportLine -> {
+                response.append("<tr>");
+                response.append("<td class=\"tagname\">").append(aggregatedTestReportLine.feature).append("</td>");
+                aggregatedTestReportLine.getTestReportLines().forEach(testReportLine ->
+                    response.append("<td class=\"")
+                        .append(testReportLine.status.toLowerCase())
+                        .append("\">")
+                        .append(testReportLine.status)
+                        .append("</td>")
+                );
+                response.append("</tr>");
+            });
+            response.append("</table>");
+            response.append("</body>");
+            response.append("</html>");
+
+            context.render(response.toString());
+        }
+
+        private AggregatedTestReportLine createAggregatedTestReportLine(
+            List<? extends TestReport> testReports,
+            String feature
+        ) {
+            List<TestReportLine> allTestReportLinesForThisFeature = testReports.stream()
+                .map(testReport -> testReport.getTestReportLineByFeature(feature))
+                .collect(toList());
+
+            return new AggregatedTestReportLine(feature, allTestReportLinesForThisFeature);
         }
 
     }
