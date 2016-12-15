9a3ccfd8054130963fd345f699c9f7d1f8c80614
Kreyling, Thomas (ext) <thomas.kreyling.ex@easycredit.de>
2016-12-15 14:32:03 +0100

    added serving of css files

--- src/main/java/kreyling/cragg/Main.java
+++ src/main/java/kreyling/cragg/Main.java
@@ -1,13 +1,33 @@
 package kreyling.cragg;
 
+import ratpack.handling.Context;
+import ratpack.http.client.HttpClient;
+import ratpack.server.BaseDir;
 import ratpack.server.RatpackServer;
 
+import java.net.URI;
+
 public class Main {
+
+    public static final String JENKINS_REPORT = "https://jenkins.easycredit.intern/view/KW-B2B/view/kwb2b/job/kwb2b-tests/1322/cucumber-html-reports/";
+
     public static void main(String... args) throws Exception {
         RatpackServer.start(server -> server
+            .serverConfig(c -> c.baseDir(BaseDir.find()).build())
             .handlers(chain -> chain
-                .get(ctx -> ctx.render("Hello " + ctx.getRequest().getQueryParams().get("name") + "!"))
+                    .files(files -> files.dir("static"))
+                    .get(Main::handleRequest)
             )
         );
+    }
+
+    private static void handleRequest(Context ctx) {
+        HttpClient httpClient = ctx.get(HttpClient.class);
+        URI uri = URI.create(JENKINS_REPORT + "feature-overview.html");
+
+        httpClient.get(uri).then(response -> {
+            ctx.getResponse().status(response.getStatus()).contentType(response.getBody().getContentType().toString());
+            ctx.render(response.getBody().getText());
+        });
     }
 }
